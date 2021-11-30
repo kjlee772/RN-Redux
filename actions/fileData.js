03e6e02afb005ce6ca3514282954c58d9d009257
kjlee772 <kjlee50612@gmail.com>
2021-11-30 22:09:46 +0900

    redux action

--- actions/fileData.js
+++ actions/fileData.js
@@ -1,6 +1,19 @@
 export const CHN_FILE_URI = 'fileData/CHN_FILE_URI';
-export const CHN_FILE_BASE64 = 'fileData/CHN_FILE_BASE64';
 export const CHN_FILE_NAME = 'fileData/CHN_FILE_NAME';
+export const CHN_OCR_RESULT = 'fileData/CHN_OCR_RESULT';
+export const CHK_KEY = 'fileData/CHK_KEY';
+export const CHK_ALL_KEY = 'fileData/CHK_ALL_KEY';
+export const CHK_SUBJECT = 'fileData/CHK_SUBJECT';
+export const CHK_SUMMARY = 'fileData/CHK_SUMMARY';
+
+export const MAKE_LIST = 'fileData/MAKE_LIST';
+
+export const make_list = (para) => {
+  return {
+    type: MAKE_LIST,
+    payload: para,
+  }
+}
 
 export const chn_file_uri = (uri) => {
   return {
@@ -9,16 +22,44 @@ export const chn_file_uri = (uri) => {
   }
 };
 
-export const chn_file_base64 = (base64) => {
-  return {
-    type: CHN_FILE_BASE64,
-    payload: base64,
-  }
-};
-
 export const chn_file_name = (name) => {
   return {
     type: CHN_FILE_NAME,
     payload: name,
+  }
+};
+
+export const chn_ocr_result = (result) => {
+  return {
+    type: CHN_OCR_RESULT,
+    payload: result,
+  }
+};
+
+export const chk_key = (key) => {
+  return {
+    type: CHK_KEY,
+    payload: key,
+  }
+};
+
+export const chk_all_key = (keys) => {
+  return {
+    type: CHK_ALL_KEY,
+    payload: keys,
+  }
+};
+
+export const chk_subject = (subject) => {
+  return {
+    type: CHK_SUBJECT,
+    payload: subject,
+  }
+};
+
+export const chk_summary = (summary) => {
+  return {
+    type: CHK_SUMMARY,
+    payload: summary,
   }
 };
